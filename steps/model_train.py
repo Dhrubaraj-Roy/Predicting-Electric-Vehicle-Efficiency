fca45b75076e9d21266f1e9a9f85f21ae62122bb
Dhrubaraj Roy <dhrubarajroy123@gmail.com>
2023-09-15 01:56:04 +0530

    files added

---
+++ steps/model_train.py
@@ -0,0 +1,53 @@
+import logging
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from zenml import step
+from zenml.artifacts import Model
+from zenml.exceptions import InputError
+
+# Define constants for data splitting
+TEST_SIZE = 0.2
+RANDOM_STATE = 42
+
+@step
+def train_model(data: pd.DataFrame) -> Model:
+    """
+    Train a linear regression model using the provided data.
+
+    Args:
+        data (pd.DataFrame): The training data.
+
+    Returns:
+        Model: The trained machine learning model.
+    """
+    try:
+        # Split data into features (X) and target (y)
+        X = data.drop(columns=['target_column'])  # Replace 'target_column' with your actual target column name
+        y = data['target_column']  # Replace 'target_column' with your actual target column name
+
+        # Split data into training and testing sets
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=TEST_SIZE, random_state=RANDOM_STATE)
+
+        # Initialize and train a linear regression model
+        model = LinearRegression()
+        model.fit(X_train, y_train)
+
+        # Make predictions on the test set
+        y_pred = model.predict(X_test)
+
+        # Calculate the Mean Squared Error (MSE) as a performance metric
+        mse = mean_squared_error(y_test, y_pred)
+
+        # Log the MSE
+        logging.info(f"Mean Squared Error (MSE): {mse}")
+
+        # Create a ZenML Model artifact
+        ml_model = Model().from_estimator(model)
+
+        return ml_model
+
+    except Exception as e:
+        logging.error(f"Error during model training: {e}")
+        raise InputError(e)
